e56232b969a1f3d96015e0b8ca2bbacf2f733a61
Régys Borges da Silveira <regys.silveira@gmail.com>
2020-04-25 17:21:06 -0300

    - calculo de percentuais por milhão da população no frames de dados

--- principal.datamodule.pas
+++ principal.datamodule.pas
@@ -17,6 +17,7 @@ type
   end;
 
   TFrameHelper = class helper for TFrame
+    procedure PreencherLabelPercentual(const ADataSet: TDataset; const AFieldCasos: string);
     procedure Preencher(const ADataSet: TDataset);
   end;
 
@@ -133,6 +134,29 @@ end;
 
 { TFrameHelper }
 
+procedure TFrameHelper.PreencherLabelPercentual(const ADataSet: TDataset; const AFieldCasos: string);
+var
+  LabelCalc: TComponent;
+  CampoCasos: TField;
+  PercCasos: Double;
+begin
+  CampoCasos := ADataSet.FindField(AFieldCasos);
+  if Assigned(CampoCasos) then
+  begin
+    LabelCalc := Self.FindComponent(AFieldCasos);
+    if Assigned(LabelCalc) then
+    begin
+      try
+        PercCasos := (CampoCasos.AsFloat * 100) / 1000000;
+      except
+        PercCasos := 0.00;
+      end;
+
+      (LabelCalc as TLabel).Text := FormatFloat(',#0.000%', PercCasos);
+    end;
+  end;
+end;
+
 procedure TFrameHelper.Preencher(const ADataSet: TDataset);
 var
   I: Integer;
@@ -149,6 +173,10 @@ begin
         (Controle as TLabel).Text := Campo.AsNumFormatado;
     end;
   end;
+
+  Self.PreencherLabelPercentual(ADataSet, 'casesPerOneMillion');
+  Self.PreencherLabelPercentual(ADataSet, 'deathsPerOneMillion');
+  Self.PreencherLabelPercentual(ADataSet, 'testsPerOneMillion');
 end;
 
 { TDtmPrincipal }
